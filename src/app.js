2dac6510c772ec7341375cd5c8d53705e2aac123
vivekdhapa <vivekdhapa24@gmail.com>
2026-02-21 21:32:51 +0530

    healthcheck routes

--- src/app.js
+++ src/app.js
@@ -18,4 +18,16 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({ extended : true, limit:"16kb"}))
 app.use(express.static("public")) //used to serve any static files eg. html css image video pdf etc
 
+//import routes
+import heathcheckRouter from "./routes/healthcheck.routes.js"
+
+
+
+//routes
+app.use("/api/v1/healthcheck",heathcheckRouter)
+
+
+
+
+
 export { app }
---
+++ src/routes/healthcheck.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { healthcheck } from "../controllers/healthcheck.controllers.js";
+
+const router=Router()
+// /api/v1/healthcheck/test
+
+router.route("/").get(healthcheck)  //get req
+
+
+export default router
