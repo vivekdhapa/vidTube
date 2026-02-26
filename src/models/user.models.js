34048ba668b16440933f31056bdd30291c183b58
vivekdhapa <vivekdhapa24@gmail.com>
2026-02-26 16:44:12 +0530

    add access and refresh tokens using jwt

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -16,6 +16,8 @@ users [icon: user] {
 
 
 import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema =new Schema(
     {
@@ -66,5 +68,45 @@ const userSchema =new Schema(
     },
     {timestamps: true}
 )
+
+userSchema.pre("save", async function (next){
+    if(!this.modified("password") )return next() //we dont want to encrypt password everytime but only when password is updated
+    this.password=bcrypt.hash(this.password,10) 
+    next()
+})
+//now we'll compare the encryped password with the user entered password
+
+userSchema.methods.isPasswordCorrect=async function(password){
+   return await bcrypt.compare(password,this.password)
+}
+
+userSchema.method.generateAccessToken= function (){
+    //short lived access token
+    return jwt.sign({
+//stored users in db's info
+       _id: this._id,
+       email: this.email,
+       username: this.username,
+       fullname : this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,    
+    { expiresIn:process.env.ACCESS_TOKEN_EXPIRY}
+    );
+}
+userSchema.method.generateRefreshToken= function (){
+    //long lived token
+    return jwt.sign({
+//stored users in db's info
+       _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,    
+    { expiresIn:process.env.REFRESH_TOKEN_EXPIRY}
+    );
+}
+
+
+
+
+
 
 export const User = mongoose.model("User",userSchema)
